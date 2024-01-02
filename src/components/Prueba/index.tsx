e6d2daba565320e6a75b6374f0ff1794a9a07b4d
Erick Guerra <erickguerra2201@gmail.com>
2024-01-02 10:06:36 -0600

    Form fixes

--- src/components/Prueba/index.tsx
+++ src/components/Prueba/index.tsx
@@ -52,11 +52,14 @@ const useStyles = makeStyles((theme: Theme) => ({
     title: {
         fontFamily: 'times',
         fontStyle: 'italic',
-        fontSize: '3em',
+        fontSize: '2.4em',
         margin: 0,
         marginBottom: '10px',
         fontWeight: '500',
         color: '#AA1936',
+        display: 'flex',
+        alignItems: 'center',
+        columnGap: '10px',
     },
 }));
 
@@ -64,8 +67,16 @@ const ContactInfo = () => {
     const classes = useStyles();
 
     return (
-        <div style={{ marginTop: '10%'}}>
-              <h1 className={classes.title}><img src="https://i.ibb.co/k58Z8Jr/school-FILL0-wght400-GRAD0-opsz48-2x.png" style={{ width: '4%'}} alt="Icono" /> Descubre a parte del claustro</h1>
+        <div style={{ marginTop: '10%' }}>
+            <h1 className={classes.title}>
+                <img src="https://i.ibb.co/k58Z8Jr/school-FILL0-wght400-GRAD0-opsz48-2x.png" style={{ width: '4%' }} alt="Icono" />
+                Hacia una comunicación interna efectiva
+            </h1>
+            <p>
+                ¿Sabías que la comunicación eficaz promueve el compromiso con los equipos y las organizaciones? ¿Deseas fortalecer tus conocimientos y competencias de comunicación interna estratégica?
+                <br /><br />
+                Conoce cómo <strong>aplicar herramientas de comunicación estratégica para mejorar la cultura corporativa y los resultados del negocio.</strong> Y consigue que las Políticas de Recursos Humanos sean mejor comprendidas, valoradas y, por tanto, aplicadas por todos los profesionales de la organización. 
+            </p>
         </div>
     )
 }
