f0ca4b4fa4dcdc2d18fd90fa8e3369b52bc54209
Amira <amiramandisbaa@gmail.com>
2024-01-02 17:49:56 +0100

    bodyinfo2

--- src/components/BodyInfo2/index.tsx
+++ src/components/BodyInfo2/index.tsx
@@ -79,7 +79,7 @@ const BodyInfo2 = () => {
             <FontAwesomeIcon icon={faQuoteLeft} className={classes.icon} />
             En estos tiempos en los que se extienden fenómenos como el de<br />la “renuncia silenciosa” (“quiet quitting”), una certeza se<br />consolida: para ser diferencialmente competitivas, las<br />organizaciones precisan de profesionales comprometidos. Y el<br />compromiso se construye, entre otros pilares, sobre una<br />comunicación eficaz. <FontAwesomeIcon icon={faQuoteRight} className={classes.icon} />
           </p>
-          <p className={classes.name}>Pablo Gonzalo Molina</p>
+          <p className={classes.name}> <span>Pablo Gonzalo Molina</span> <br />Socio de Estudio de Comunicación <br /> Director de programa ejecutivo HR: Comunicación Interna al servicio del Negocio</p>
           {/* ... (añade más contenido según sea necesario) */}
         </div>
       </div>
